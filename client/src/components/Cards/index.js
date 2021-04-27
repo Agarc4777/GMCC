1819394a37cc8df7e11346857b1f0f9ee325c32d
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-04-26 21:59:55 -0500

    added fixed images to the cards.

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,60 +1,60 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
+import CardGroup from 'react-bootstrap/CardGroup';
 import Phicon from "../../images/phicon.png";
+import OilChange from "../../images/oil-change-picture.jpg";
+import CarLifted from "../../images/car-on-lift.jpg";
+import Button from 'react-bootstrap/Button'
 import "./style.css"
 
 
 
 function Cards() {
   return(
-    <div className="container">
-        <CardDeck>
-            <Card>
-                <Card.Img id="image" variant="right" src={Phicon} />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Link goes here</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img id="image" variant="mid" src={Phicon} />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Link goes here</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img id="image" variant="end" src={Phicon} />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Link goes here</small>
-                </Card.Footer>
-            </Card>
-        </CardDeck>
-    </div>
-    
+    <CardGroup>
+  <Card className="card">
+    <Card.Img variant="top" id="image" src={ OilChange } />
+    <Card.Body>
+      <Card.Title>Preventative Maintenance</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button href="#">Learn More</Button>
+    </Card.Footer>
+  </Card>
+  <Card className="card">
+    <Card.Img variant="top" id="image" src={ CarLifted } />
+    <Card.Body>
+      <Card.Title>Auto Repair Services</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button href="#">Learn More</Button>
+    </Card.Footer>
+  </Card>
+  <Card className="card">
+    <Card.Img variant="top" id="image" src={ Phicon } />
+    <Card.Body>
+      <Card.Title>Schedule Your Appointment</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button href="#">Schedule Now!</Button>
+    </Card.Footer>
+  </Card>
+</CardGroup>
   )
 }
 
